0c710f4f9d84b4f47bef244efb9cddf9807f14ad
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-11-11 22:24:24 -0300

    supports.index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SupportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/supports", [SupportController::class, 'index'])->name('supports.index');
+
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('supports.index');
 });
