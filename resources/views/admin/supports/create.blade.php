fa249ab9705aeae0e737fec819106cf4030f8abe
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-11-13 23:10:55 -0300

    foco no input subject

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -10,7 +10,7 @@
 
 <form action="{{ route('supports.store') }}" method="POST">
     @csrf
-    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') ?? '' }}">
+    <input autofocus type="text" name="subject" placeholder="Assunto" value="{{ old('subject') ?? '' }}">
     <textarea name="body"  cols="30" rows="5" placeholder="Descrição">{{ old('body') ?? '' }}</textarea>
     <button type="submit">Incluir Dúvida</button>
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -2,7 +2,7 @@
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
     @csrf
     @method('PUT')
-    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') ?? $support->subject }}">
+    <input autofocus type="text" name="subject" placeholder="Assunto" value="{{ old('subject') ?? $support->subject }}">
     <textarea name="body"  cols="30" rows="5" placeholder="Descrição">{{ old('body') ?? $support->body }}</textarea>
     <button type="submit">Salvar Dúvida</button>
 </form>
