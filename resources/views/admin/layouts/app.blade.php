147ed1e7d8bb61ef85ae13a16d93bc1cae486848
iFHA <fernando.everywhere@gmail.com>
2023-11-15 21:10:38 -0300

    layout de listagem dos suportes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <section class="container px-4 mx-auto">
+    <section class="container px-4 mx-auto mb-7">
         @yield('header')
         <div>
             <x-messages/>
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,36 +1,11 @@
-@extends("admin.layouts.app", ['title'=>'Listagem dos Suportes'])
+@extends("admin.layouts.app", ['title'=>'Fórum'])
+
 @section("header")
 @include('admin.supports.partials.header', compact('supports', 'filters'))
 @endsection
-@section("content")
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
-<table>
-    <thead>
-        <tr>
-            <th>Assunto</th>
-            <th>Descrição</th>
-            <th>Status</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ $support->body }}</td>
-                <td>{{ $support->status }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">
-                        >
-                    </a>
-                    <a href="{{ route('supports.edit', $support->id) }}">
-                        Editar
-                    </a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-<x-pagination :paginator="$supports" :appends="$filters"/>
 
+@section("content")
+@include('admin.supports.partials.content', compact('supports'))
+
+<x-pagination :paginator="$supports" :appends="$filters"/>
 @endsection
