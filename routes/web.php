7c381414ff61c5bf34858e6e48ff018aff07a649
iFHA <fernando.everywhere@gmail.com>
2023-11-16 00:11:39 -0300

    breeze instalado e utilizando autenticação nas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +15,26 @@ use App\Http\Controllers\Admin\SupportController;
 |
 */
 
-Route::get("/supports", [SupportController::class, 'index'])->name('supports.index');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::post('/supports/store', [SupportController::class, 'store'])->name('supports.store');
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::put('/supports/{id}/update', [SupportController::class, 'update'])->name('supports.update');
-
 Route::get('/', function () {
     return to_route('supports.index');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get("/supports", [SupportController::class, 'index'])->name('supports.index');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::post('/supports/store', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::put('/supports/{id}/update', [SupportController::class, 'update'])->name('supports.update');
+
+});
+
+require __DIR__.'/auth.php';
