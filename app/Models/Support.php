b9bb60d30e5c73c680a8e84162cec70362f6b673
iFHA <fernando.everywhere@gmail.com>
2023-11-30 18:50:53 -0300

    add replysupport

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Support extends Model
 {
@@ -20,5 +22,13 @@ class Support extends Model
     }
     public function getStatusDescriptionAttribute(): string {
         return SupportStatus::tryFrom($this->status)->getDescription();
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies(): HasMany {
+        return $this->hasMany(ReplySupport::class);
     }
 }
